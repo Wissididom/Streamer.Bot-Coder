81e6aac2c3a58db0e0e6ae1eccb417586df02925
Wissididom <30803034+Wissididom@users.noreply.github.com>
2025-01-06 17:31:24 +0100

    Initial deno commit

---
+++ encode.ts
@@ -0,0 +1,21 @@
+import zlib from "node:zlib";
+import { Buffer } from "node:buffer";
+
+export function encodeCustomUUencodedText(json: string): string {
+  const prettyfiedJson = JSON.stringify(JSON.parse(json), null, 2).replace(/\n/g, "\r\n").replace(/"weight": ?(\d+),/g, '"weight": $1.0,');
+
+  // Compress using gzip
+  const compressedData = zlib.gzipSync(prettyfiedJson);
+
+  // Add the non-gzip prefix 'SBAE'
+  let binaryData = Buffer.from("SBAE", "ascii");
+  binaryData = Buffer.concat([binaryData, compressedData]);
+
+  // Encode to base64
+  return binaryData.toString("base64");
+}
+
+if (import.meta.main) {
+  const encodedText = encodeCustomUUencodedText("TODO");
+  console.log(encodedText);
+}
